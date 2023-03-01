d1a9b77acecd38ba94968e194020c635f69fa8c1
JiheokCHOI <113998002+JiheokCHOI@users.noreply.github.com>
2023-03-01 15:46:21 +0900

    Add files via upload

---
+++ todo-kotlin/src/main/kotlin/com/CHOIJiheok/kotlinspring/todo/TodoApplication.kt
@@ -0,0 +1,11 @@
+package com.CHOIJiheok.kotlinspring.todo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TodoApplication
+
+fun main(args: Array<String>) {
+    runApplication<TodoApplication>(*args)
+}
